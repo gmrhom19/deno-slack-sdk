c13bf9eebe33c3dcbc5bfe9fb5a8a41f94925c88
Filip Maj <fmaj@slack-corp.com>
2023-03-15 15:27:16 -0400

    commit scratchpad screenshare random bits.

--- src/types/types_test.ts
+++ src/types/types_test.ts
@@ -1,6 +1,7 @@
 import { DefineType } from "./mod.ts";
 import { assertEquals } from "../dev_deps.ts";
 import { CustomType } from "./types.ts";
+import { DefineProperty } from "../mod.ts";
 
 Deno.test("DefineType test against id using the name parameter", () => {
   const Type = DefineType({
@@ -57,23 +58,23 @@ Deno.test("CustomType applied to DefineType object should provide a proper type"
       name: {
         type: "string",
       },
-      field: {
+      field: DefineProperty({
         type: "object",
         properties: {
           name2: {
-            type:"string"
-          }
+            type: "string",
+          },
         },
-        required: []
-      },
+        required: [],
+      }),
     },
-    required: []
+    required: [],
   });
 
   type mytype = CustomType<typeof Type>;
 
   const test = (thing: mytype) => {
-    thing.field.;
+    thing.field;
   };
 });
 
@@ -115,6 +116,6 @@ Deno.test("CustomType applied to DefineType object should provide a proper type"
   type mytype = CustomType<typeof Type>;
 
   const test = (thing: mytype) => {
-    thing.type;
+    thing;
   };
 });
